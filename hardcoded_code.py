4e43bd23dbf0681c06be8fedcb82f8595198b3f5
sinner995 <46018881+sinner995@users.noreply.github.com>
2019-11-16 12:55:24 +0300

    Update hardcoded_code.py
    Добавил вторую задачу, которая решается неправильно и выводит неверный ответ.

--- hardcoded_code.py
+++ hardcoded_code.py
@@ -12,16 +12,15 @@ else:
 
 login_data = dict(csrfmiddlewaretoken=csrftoken, login='test_lab_2', password='Lab2Test', next='/')
 response = client.post(URL, data=login_data, headers=dict(Referer=URL))
-print(response)
-print("__________________________________________________________________________________________")
+print("User Authorization Status:", response)
 
+print("\nTask Add Digits")
 URL2 = 'https://leetcode.com/problems/add-digits/'
 
 cj = browser_cookie3.chrome()
 r = client.get(URL2, cookies=cj)
 c_a_u = r.cookies.values()[0]
-print(c_a_u)
-print("__________________________________________________________________________________________")
+# print(c_a_u)
 
 URL3 = 'https://leetcode.com/problems/add-digits/submit/'
 
@@ -40,8 +39,7 @@ __cfduid = '__cfduid='+cookie.values()[1]
 token = 'csrftoken='+csrftoken
 LEETCODE_SESSION = 'LEETCODE_SESSION='+cookie.values()[0]
 l = str(__cfduid+'; '+token+'; '+LEETCODE_SESSION+'; '+c_a_u)
-print(l)
-print("__________________________________________________________________________________________")
+# print(l)
 
 s = {
     'authority': 'leetcode.com',
@@ -63,11 +61,10 @@ s = {
 }
 
 response = client.post(URL3,  json=problems_login, headers=s)
-print(response)
-print(response.text)
+print("Task Transfer Status:", response)
+# print(response.text)
 submit_id = re.search(': (.*?)}', response.text).group(1)
-print(submit_id)
-print("__________________________________________________________________________________________")
+# print(submit_id)
 
 
 URL4 = str('https://leetcode.com/submissions/detail/'+submit_id+'/check/')
@@ -77,6 +74,65 @@ while True:
     client.get(URL5)
     h = client.get(URL4)
     if '{"state": "STARTED"}' != h.text and '{"state": "PENDING"}' != h.text:
-        print(h.json())
+        print("Information:", "\n\tTask completed correctly:", h.json()["run_success"], "\n\tRuntime:", h.json()["status_runtime"], "\n\tMemory:", h.json()["memory"])
         break
 
+
+print("\nTask Defanging An Ip Address")
+
+URL6 = 'https://leetcode.com/problems/defanging-an-ip-address/'
+
+cj = browser_cookie3.chrome()
+r = client.get(URL6, cookies=cj)
+c_a_u = r.cookies.values()[0]
+
+URL7 = 'https://leetcode.com/problems/defanging-an-ip-address/submit/'
+
+if 'csrftoken' in client.cookies:
+    csrftoken = client.cookies['csrftoken']
+else:
+    csrftoken = client.cookies['csrf']
+
+problems_login = {"question_id":"1205",
+                  "lang":"python3",
+                  "typed_code":"class Solution:\n    def defangIPaddr(self, address: str) -> str:\n        "}
+
+cookie = client.cookies
+
+__cfduid = '__cfduid='+cookie.values()[1]
+token = 'csrftoken='+csrftoken
+LEETCODE_SESSION = 'LEETCODE_SESSION='+cookie.values()[0]
+l = str(__cfduid+'; '+token+'; '+LEETCODE_SESSION+'; '+c_a_u)
+# print(l)
+
+s = {
+    'authority': 'leetcode.com',
+    'method': 'POST',
+    'path': '/problems/defanging-an-ip-address/submit/',
+    'scheme': 'https',
+    'accept': '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'content-length': '130',
+    'content-type': 'application/json',
+    'cookie': l,
+    'origin': 'https://leetcode.com',
+    'referer': 'https://leetcode.com/problems/defanging-an-ip-address/submissions/',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+    'x-csrftoken': csrftoken
+}
+
+response = client.post(URL7,  json=problems_login, headers=s)
+print("Task Transfer Status:", response)
+# print(response.text)
+submit_id = re.search(': (.*?)}', response.text).group(1)
+# print(submit_id)
+
+while True:
+    client.get(URL5)
+    h = client.get(URL4)
+    if '{"state": "STARTED"}' != h.text and '{"state": "PENDING"}' != h.text:
+        print("Information:", "\n\tTask completed correctly:", h.json()["run_success"], "\n\tRuntime:", h.json()["status_runtime"], "\n\tMemory:", h.json()["memory"])
+        break
